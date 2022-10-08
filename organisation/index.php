f5d4f7a11971e4829dae547397bb4012a2a1cc3d
Bohdan <bogmira777@gmail.com>
2022-10-08 13:44:10 +0200

    Organisation task fix

--- organisation/index.php
+++ organisation/index.php
@@ -2,7 +2,7 @@
 
 $letter = "«Мы будем рады видеть Вашего сына на нашем мероприятии. Ждем его 25 октября. Оргкомитет.»";
 $pieces = explode(". ", $letter);
-$pieces[0] = substr_replace($pieces[0], "Уважаемый Slavik! ", 1, 0);
+$pieces[0] = substr_replace($pieces[0], "Уважаемый Slavik! ", 2, 0);
 $pieces[2] = str_replace("Оргкомитет", "Администрация", $pieces[2]);
 $pieces[0] = str_replace("Вашего сына", "Вашу дочь", $pieces[0]);
 $pieces[1] = str_replace("его", "ее", $pieces[1]);
